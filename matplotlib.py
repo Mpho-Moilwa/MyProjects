0585f9fca0f56b50660902d471843cf0eabfbf3a
Mpho-Moilwa <moilwampho2@gmail.com>
2026-07-09 13:00:51 +0200

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 5, 3]
+
+plt.figure()
+plt.plot(x, y)
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.title("Example Line Plot")
+plt.show()
+
+
